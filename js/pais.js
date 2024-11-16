ba3171f4e837c81ab4d50e6342e637be267c4936
uo294197 <148157735+uo294197@users.noreply.github.com>
2024-11-16 20:42:14 +0100

    Update pais.js

--- js/pais.js
+++ js/pais.js
@@ -11,7 +11,7 @@ class Pais {
         this.tipo = "&mode=xml";
         this.unidades = "&units=metric";
         this.idioma = "&lang=es"; 
-        this.url = "http://api.openweathermap.org/data/2.5/forecast?lat=" + this.latMeta + "&lon=" + this.longMeta + this.tipo + this.unidades + this.idioma + "&appid=" + this.apikey;
+        this.url = "http://api.openweathermap.org/data/2.5/weather?q=" + this.ciudad + this.tipo + this.unidades + this.idioma + "&APPID=" + this.apikey;
     }
 
     getNombreCircuito() {
