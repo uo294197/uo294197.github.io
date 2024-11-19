fd9ac585227b1dd3d169a2a9e8dc4e17abed7a97
uo294197 <148157735+uo294197@users.noreply.github.com>
2024-11-19 09:10:35 +0100

    Update pais.js

--- js/pais.js
+++ js/pais.js
@@ -2,68 +2,63 @@
 class Pais{
     constructor(nombreCircuito,latMeta, longMeta){
        this.nombreCircuito=nombreCircuito;
-	this.latMeta = latMeta;
+	   this.latMeta = latMeta;
       this.longMeta = longMeta;
     }
 	
     setValores(){
-	this.apikey = "9b78bae472a2bd2d0d112053ad77a805";
-        this.ciudad = "Shanghai";
-        this.codigoPais = "CN";
+		this.apikey = "9b78bae472a2bd2d0d112053ad77a805";
+		this.ciudad = this.nombreCircuito;
+        this.tipo = "&mode=xml";
         this.unidades = "&units=metric";
         this.idioma = "&lang=es";
-	this.url = "https://api.openweathermap.org/data/2.5/weather?q=" + this.ciudad + this.unidades + this.idioma + "&APPID=" + this.apikey;        
-        this.mostrarTiempo();
+        this.url = "http://api.openweathermap.org/data/2.5/forecast?lat=" + this.latMeta+"&lon="+this.longMeta + "mode="+this.tipo + this.unidades + this.idioma + "&appid=" + this.apikey;
     }
     
     getNombreCircuito(){
         return this.nombreCircuito
     }
    
-   mostrarTiempo(){
+ 
+	 cargarDatos(){
         $.ajax({
-            dataType: "json",
+            dataType: "xml",
             url: this.url,
             method: 'GET',
-            success: function(datos) 
-            {  
-                $("h3").remove();
-                $("article").remove();
-                var icono = datos.weather[0].icon;
-                var imagen = "https://openweathermap.org/img/w/" + icono + ".png";
-                var stringDatos = "<article>";
-                stringDatos += "<h2>Datos</h2>";                    
-                stringDatos += "<img src=\"" + imagen + "\" alt=\"Icono del tiempo\"/>";
-                stringDatos += "<p>Ciudad: " + datos.name + "</p>";
-                stringDatos += "<p>País: " + datos.sys.country + "</p>";
-                stringDatos += "<p>Latitud: " + datos.coord.lat + " grados</p>";
-                stringDatos += "<p>Longitud: " + datos.coord.lon + " grados</p>";
-                stringDatos += "<p>Temperatura: " + datos.main.temp + " grados Celsius</p>";
-                stringDatos += "<p>Temperatura máxima: " + datos.main.temp_max + " grados Celsius</p>";
-                stringDatos += "<p>Temperatura mínima: " + datos.main.temp_min + " grados Celsius</p>";
-                stringDatos += "<p>Presión: " + datos.main.pressure + " milímetros</p>";
-                stringDatos += "<p>Humedad: " + datos.main.humidity + "%</p>"; 
-                stringDatos += "<p>Amanece a las: " + new Date(datos.sys.sunrise *1000).toLocaleTimeString() + "</p>"; 
-                stringDatos += "<p>Oscurece a las: " + new Date(datos.sys.sunset *1000).toLocaleTimeString() + "</p>"; 
-                stringDatos += "<p>Dirección del viento: " + datos.wind.deg + "  grados</p>";
-                stringDatos += "<p>Velocidad del viento: " + datos.wind.speed + " metros/segundo</p>";
-                stringDatos += "<p>Hora de la medida: " + new Date(datos.dt *1000).toLocaleTimeString() + "</p>";
-                stringDatos += "<p>Fecha de la medida: " + new Date(datos.dt *1000).toLocaleDateString() + "</p>";
-                stringDatos += "<p>Descripción: " + datos.weather[0].description + "</p>";
-                stringDatos += "<p>Visibilidad: " + datos.visibility + " metros</p>";
-                stringDatos += "<p>Nubosidad: " + datos.clouds.all + " %</p>";                    
-                stringDatos += "</article>";
-                $("input").after(stringDatos);
-            },
-            error:function()
-            {   
-                $("h3").remove();
-                $("article").remove();
-                $("input:last").after("<h3>Ha ocurrido un error, no se han podido cargar los datos</h3>");
-            }
+            success: function(datos){
+                
+                  
+                    $("h5").text((new XMLSerializer()).serializeToString(datos));
+                
+           
+                   
+                    var ciudad                = $('location',datos).attr("name");
+                    var temperaturaMin        = $('temperature',datos).attr("min");
+                    var temperaturaMax        = $('temperature',datos).attr("max");
+                    var humedad               = $('humidity',datos).attr("value");
+                    var precipitacionValue    = $('precipitation',datos).attr("value");
+					var symbol    = $('symbol',datos).attr("var");
+                    
+					
+                    var stringDatos =   "<li>Ciudad: " + ciudad + "</li>";
+                    stringDatos += "<li>Temperatura mÃ­nima: " + temperaturaMin + " grados Celsius</li>";
+                    stringDatos += "<li>Temperatura mÃ¡xima: " + temperaturaMax + " grados Celsius</li>";
+                    stringDatos += "<li>Humedad: " + humedad + " " + humedadUnit + "</li>";
+                    stringDatos += "<li>PrecipitaciÃ³n valor: " + precipitacionValue + "</li>";
+                    $("p").html(stringDatos);                  
+                },
+            error:function(){
+                $("h3").html("Â¡Tenemos problemas! No puedo obtener XML de <a href='http://openweathermap.org'>OpenWeatherMap</a>"); 
+                $("h4").remove();
+                $("h5").remove();
+                $("p").remove();
+                }
         });
-   }
+    }
     crearElemento(tipoElemento, texto, insertarAntesDe){
+        // Crea un nuevo elemento modificando el Ã¡rbol DOM
+        // El elemnto creado es de 'tipoElemento' con un 'texto' 
+        // El elemnto se coloca antes del elemnto 'insertarAntesDe'
         var elemento = document.createElement(tipoElemento); 
         elemento.innerHTML = texto;
         $(insertarAntesDe).before(elemento);
@@ -75,9 +70,11 @@ class Pais{
         this.crearElemento("h5","","footer"); // Crea un elemento con DOM para el string con XML
         this.crearElemento("h4","Datos","footer"); // Crea un elemento con DOM 
         this.crearElemento("p","","footer"); // Crea un elemento con DOM para los datos obtenidos con XML
-        this.mostrarTiempo();
+        this.cargarDatos();
+        $("button").attr("disabled","disabled");
     }
 }
 
 var pais = new Pais("Shanghai",31.336667, 121.218611)
+pais.verXML();
 
